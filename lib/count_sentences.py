e390275f3ffb7035fd6867cbdbf12548084a432f
Shubhkarman Singh Rathore <shubhkarman4733@yahoo.in>
2023-07-19 16:04:17 -0700

    adds lab

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -1,4 +1,48 @@
-#!/usr/bin/env python3
+import re
+
 
 class MyString:
-  pass
+    def __init__(self, value=""):
+        self.value = value
+
+    def get_value(self):
+        return self._value
+
+    def set_value(self, value):
+        if isinstance(value, str):
+            self._value = value
+        else:
+            print("The value must be a string.")
+
+    value = property(get_value, set_value)
+
+    def is_sentence(self):
+        if self.value.endswith("."):
+            return True
+        else:
+            return False
+
+    def is_question(self):
+        if self.value.endswith("?"):
+            return True
+        else:
+            return False
+
+    def is_exclamation(self):
+        if self.value.endswith("!"):
+            return True
+        else:
+            return False
+
+    def count_sentences(self):
+        # Using regular expression to split the value into sentences
+        sentences = re.split(r"[.!?]", self.value)
+        # Removing empty strings resulting from multiple consecutive sentence separators
+        sentences = [sentence for sentence in sentences if sentence.strip()]
+        return len(sentences)
+
+
+# Example usage:
+string = MyString()
+string.value = "This is a string! It has three sentences. Right?"
+print(string.count_sentences())  # Output: 3
